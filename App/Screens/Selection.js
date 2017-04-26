d17e43277d9fd0bf277729ee5269b54c66088871
H. Cole Wiley <cole@scandy.co>
2017-04-26 13:24:59 -0500

    added react-native-fs to show how to get files from Download folder and display them as loadable meshes

--- App/Screens/Selection.js
+++ App/Screens/Selection.js
@@ -15,32 +15,96 @@ import {
   Text,
   View,
   Button,
+  Picker,
   Dimensions,
   DeviceEventEmitter,
   Linking,
 } from 'react-native';
 
+const FileSystem = require('react-native-fs');
+
+const scandy_core_test_files = [
+  { title: "Select a file", url:""},
+  { title: "View OBJ.zip", url: "https://s3.amazonaws.com/scandycore-test-assets/scandy-obj.zip" },
+  { title: "View PLY.zip", url: "https://s3.amazonaws.com/scandycore-test-assets/scandy-ply.zip" },
+  { title: "View PLY", url: "https://s3.amazonaws.com/scandycore-test-assets/scandy.ply" },
+  { title: "View OBJ", url: "https://s3.amazonaws.com/scandycore-test-assets/scandy.obj" },
+]
+
 export default class SelectionScreen extends Component {
   static navigationOptions = {
     title: 'Select Mesh',
   };
-  render() {
+
+  state = {
+    files: scandy_core_test_files,
+    selected: scandy_core_test_files[0].url
+  }
+
+  findPlyInDirectory(dir_path) {
+    let _this = this;
+    FileSystem.readDir(dir_path)
+    .then((result1) => {
+      let files = [];
+      result1.forEach(function(file) {
+        if (file.isDirectory()){
+          // Recursion!
+          _this.findPlyInDirectory(file.path);
+        } else {
+          // only grab the files we want by ext
+          if (file.path.slice((file.path.lastIndexOf('.') - 1 >>> 0) + 2) === "ply") {
+            dir_name = dir_path.split("/");
+            dir_name = dir_name[dir_name.length-1];
+            files.push({
+              title: dir_name+"/"+file.name,
+              url: file.path
+            });
+          }
+        }
+      });
+      return Promise.all(files);
+    })
+    .then(
+      (new_files) => {
+        files = _this.state.files
+        new_files.forEach((file) => {
+          files.push(file)
+        });
+        _this.setState({files: files});
+      }
+    )
+    ;
+  }
+
+  componentDidMount() {
+    this.findPlyInDirectory(FileSystem.ExternalStorageDirectoryPath + '/Download')
+  }
+
+  renderViewButtons() {
     const { navigate } = this.props.navigation;
+
+    let files = this.state.files.map( (file, i) => {
+        return <Picker.Item key={i} value={file.url} label={file.title} />
+    });
+    return (
+      <Picker
+        selectedValue={this.state.selected}
+        onValueChange={(url) => {
+          if( this.state.selected != url )
+            navigate('Viewer', { url: url})
+          }
+        }
+        >
+        { files }
+      </Picker>
+    )
+  }
+
+  render() {
     return (
       <View>
         <Text>Files to select from go here.</Text>
-        <Button
-          onPress={() => navigate('Viewer', { url: 'https://s3.amazonaws.com/scandycore-test-assets/scandy-obj.zip'})}
-          title="View OBJ.zip"
-        />
-        <Button
-          onPress={() => navigate('Viewer', { url: 'https://s3.amazonaws.com/scandycore-test-assets/scandy-ply.zip'})}
-          title="View PLY.zip"
-        />
-        <Button
-          onPress={() => navigate('Viewer', { url: 'https://s3.amazonaws.com/scandycore-test-assets/scandy.ply'})}
-          title="View PLY"
-        />
+        {this.renderViewButtons()}
       </View>
     );
   }
